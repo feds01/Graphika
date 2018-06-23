9bb1fd6775c83178067baec8e69c77f63698d89a
Alex <alexander.fedotov.uk@gmail.com>
2018-06-23 20:36:05 +0100

    'gridded' option works, new Config() class & reworked axis drawing
    The new Config() class will become a base class for using to store and create configuration data, however it could be a good idea to move away from this concept and just store the data as object attributes rather than a separate class.
    
    Implemented an efficient algorithm to draw the digit strokes, or if 'gridded' option is selected, a grid.
    
    removed redundant code such as render()
    
    added findCanvas() and findTittle(), to find the tittle div and the canvas tag within the provided html element. It could be a good idea to combine these two methods, to be more dynamic.

--- graph.js
+++ graph.js
@@ -1,5 +1,24 @@
-class BasicGraph {
+// attach key method to the json object
+if (typeof Object.keys !== 'function') {
+    Object.keys = function () {
+        let keys = [];
 
+        for (let name in this) {
+            if (Object.hasOwnProperty(name)) {
+                keys.push(name);
+            }
+        }
+        return keys;
+    }
+}
+
+function hexToOpacityHex(hex, opacity) {
+    return `#${opacity}${hex.substr(1)}`;
+}
+
+let err = "canvas element does not exist.";
+
+class BasicGraph {
     /**
      * @property x_label -> The label which is present on the x-axis of the graph
      * @property y_label -> The label which is present on the y-axis of the graph
@@ -14,113 +33,151 @@ class BasicGraph {
      * @property gridded -> if true, the graph will be drawn with lines at the intervals on the graph.
      * */
     constructor(canvas, settings, data) {
-        let clazz = this;
+        this.id = canvas;
+        this.settings = new Config(settings).config;
+        // this.data = data === undefined || null ? {'x': [], 'y': []} : data;
 
-        this.settings = {
-            x_size : 500,
-            y_size : 500,
-            x_label   : 'x-axis',
-            y_label   : 'y-axis',
-            tittle    : 'Graph',
-            tittle_pos: 'top-center',
-            scale     : 1,
-            gridded   : false,
-            padding   : 15,
-            axis_colour: 'rgba(0,0,0, 0.6)'
-        };
 
         try {
-            this.canvas = document.getElementById(canvas);
+            this.canvas = BasicGraph.findCanvas(this.id);
             this.context = this.canvas.getContext('2d');
 
             this.context.imageSmoothingEnabled = true;
             this.context.strokeStyle = this.settings.axis_colour;
 
-            this.context.font = '10px arial';
+            //this.context.font = '20px "Roboto Mono", monospace';
 
             this.settings.x_size = this.canvas.width;
             this.settings.y_size = this.canvas.height;
 
         } catch (e) {
-            if(this.canvas === null) {
-                throw ('err: provided canvas does not exist!');
+            if (this.canvas === null) {
+                throw ('err: provided canvas does not exist!\n' + e);
             }
         }
-
-        if((settings !== null) && (settings !== undefined)) {
-            for(let setting of Object.keys(settings)) {
-                this.setSetting(setting, settings[setting]);
-            }
-        }
-
-        // data settings
-        if(data === undefined || null) {
-           this.data = {'x': [], 'y': []}
-        } else {
-            this.data = data;
-        }
-        // attach key method to the json object
-
-
-        if(typeof Object.keys !== 'function') {
-            this.data.keys = function () {
-                let keys = [];
-
-                for(let name in this) {
-                    if(clazz.data.hasOwnProperty(name)) {
-                        keys.push(name);
-                    }
-                }
-                return keys;
-            }
-        } else {
-            this.data.keys = function () {
-                return Object.keys(this);
-            }
-        }
-
         // axis
+        this.setTittlePosition();
         this.drawAxis();
     }
 
-    setSetting(setting, val) {
-        if(this.settings.hasOwnProperty(setting)) {
-            this.settings[setting] = val;
+    static findCanvas(root) {
+        let element = document.getElementById(root);
+        let canvasElement = null;
+
+        if (element === null) {
+            throw err;
+        } else {
+            for (let child of element.childNodes) {
+                if (child.nodeName.toLowerCase() === 'canvas') {
+                    canvasElement = child;
+                }
+            }
+        }
+
+        if (canvasElement !== null) {
+            element.style.width = (canvasElement.width).toString() + 'px';
+        }
+        return canvasElement;
+    }
+
+    static findTittle(root) {
+        console.log(root);
+        let rootElement = document.getElementById(root);
+
+        if(rootElement === null) {
+            throw err;
+
+        } else {
+            for (let child of rootElement.childNodes) {
+                if (child.nodeName.toLowerCase() === 'div' &&
+                    child.classList.contains('tittle'))
+                {
+                    return child;
+                }
+            }
+        }
+    }
+
+    setTittlePosition() {
+        if(this.settings.tittle != null) {
+            let tittleElement = BasicGraph.findTittle(this.id);
+
+            switch (this.settings.tittle_pos) {
+                case 'top-left':
+                    tittleElement.style.textAlign = 'left';
+                    break;
+
+                case 'top-center':
+                    tittleElement.style.textAlign = 'center';
+                    break;
+
+                case 'top-right':
+                    tittleElement.style.textAlign = 'right';
+            }
+            tittleElement.innerHTML = this.settings.tittle;
         }
     }
 
     drawAxis() {
-        let y_offset = 0;
-        let padding = this.settings.padding;
-
-        if(this.settings.tittle != null &&
-           this.settings.tittle_pos.substr(0, 3) !== 'top')
-        {
-           y_offset += 10;
-        }
         // the y-limit is the y coordinate up to where everything should be drawn
-        let y_limit = this.settings.y_size - (padding + y_offset);
+        let offset = 0,
+            padding = this.settings.padding,
+            y_limit = this.settings.y_size - padding,
+            graph = {
+                x_begin: padding,
+                y_begin: padding,
+                x_end: this.settings.x_size - padding,
+                y_end: y_limit,
+                x_size: this.settings.x_size - (2 * padding),
+                y_size: y_limit - padding
+            };
 
-        this.context.strokeRect(padding, y_offset, 1, y_limit);
-        this.context.strokeRect(padding, y_limit, this.settings.x_size - 5, 1);
+        this.context.strokeRect(padding, padding, 1, graph.y_size);
+        this.context.strokeRect(padding, y_limit, graph.x_size, 1);
 
-        // now draw axis strokes
-        let counter = 0;
+        // change the stroke style to rgba(colour, 0.6), so apply 60% opacity to
+        // whataver colour is selected.
+        this.context.strokeStyle = hexToOpacityHex(this.settings.axis_colour, '99');
 
-        for(let offset = 0; offset < y_limit; offset += 20) {
-            this.context.strokeRect(padding + offset, y_limit, 1, 9);
-            this.context.strokeRect(padding, y_limit - offset, -9, 1);
+        while(offset < graph.x_size && offset < graph.y_size) {
+            let x_len = this.settings.gridded ? 9 + graph.y_size : 9,
+                y_len = this.settings.gridded ? 9 + graph.x_size : 9;
 
-            this.context.fillText(counter.toString(), padding + offset + 2, y_limit + 12);
-            this.context.fillText(counter.toString(), padding - 9, y_limit - offset);
-            counter++;
+            this.context.strokeRect(graph.x_begin + offset, graph.y_end + 9, 1, -x_len);
+            this.context.strokeRect(graph.x_begin - 9, graph.y_end - offset, y_len, 1);
+
+            offset += 20;
+        }
+    }
+}
+
+class Config {
+    constructor(config) {
+        this.config = {
+            x_size: 500,
+            y_size: 500,
+            x_label: 'x-axis',
+            y_label: 'y-axis',
+            tittle: 'Graph',
+            tittle_pos: 'top-center',
+            scale: 1,
+            gridded: false,
+            padding: 10,
+            axis_colour: '#5e5e5e'
+        };
+
+        if ((config !== null) && (config !== undefined)) {
+            for (let setting of Object.keys(config)) {
+                this.setConfig(setting, config[setting]);
+            }
         }
 
-        this.render();
+        // add config keys to the class properties
     }
 
-    render() {
-        this.context.drawImage(this.canvas, 0,0);
-        requestAnimationFrame(this.render);
+    setConfig(key, val) {
+        if (this.config.hasOwnProperty(key)) {
+            this.config[key] = val;
+        }
     }
 }
