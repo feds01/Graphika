c1c462486a7d5d67e800fdb313139f9712801ad9
Alex <alexander.fedotov.uk@gmail.com>
2018-10-01 20:36:02 +0100

    several changes.
    - added configuration file for options (graph).
    - started implementing colour schemes
    - slight optimisation in calculating max_xTicks

---
+++ src/utils/colours.js
@@ -0,0 +1,26 @@
+/**
+ * utils/colours.js Module description:
+ *
+ * This is file module is used for storing the various colours available
+ * for making graphs and charts.
+ *
+ * Created on 01/10/2018
+ * @author Alexander. E. Fedotov
+ * @email <alexander.fedotov.uk@gmail.com>
+ */
+
+module.exports = {
+    schemes: {
+        s1: ['#7fc97f', '#beaed4', '#fdc086'],
+        s2: ['#1b9e77', '#d95f02', '#7570b3'],
+        s3: ['#a6cee3', '#1f78b4', '#b2df8a'],
+        s4: ['#fbb4ae', '#b3cde3', '#ccebc5'],
+        s5: ['#b3e2cd', '#fdcdac', '#cbd5e8'],
+        s6: ['#e41a1c', '#377eb8', '#4daf4a'],
+        s7: ['#66c2a5', '#fc8d62', '#8da0cb'],
+        s8: ['#8dd3c7', '#ffffb3', '#bebada']
+    },
+
+
+};
+ 
