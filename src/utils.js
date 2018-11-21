4425a1ef6efa5a21ba41c53995ddb2454d1f1318
feds01 <alexander.fedotov.uk@gmail.com>
2018-11-21 14:25:24 +0000

    Added linting to project

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 module.exports = {
     rgba: function (hex, opacity) {
-        return hex.replace(')', `,${parseFloat((opacity / 100).toFixed(2))})`);
+        return hex.replace(")", `,${parseFloat((opacity / 100).toFixed(2))})`);
     },
 
     findObjectElements(id, options) {
@@ -12,11 +12,11 @@ module.exports = {
 
         for (let childNode of element.childNodes) {
             const tagName = childNode.nodeName.toLowerCase();
-            if (tagName === 'canvas') {
+            if (tagName === "canvas") {
                 elementMap.canvas = childNode;
 
-            } else if (tagName === 'div') {
-                if (childNode.classList.contains('tittle')) {
+            } else if (tagName === "div") {
+                if (childNode.classList.contains("tittle")) {
                     elementMap.tittle = childNode;
                 }
             }
@@ -24,22 +24,22 @@ module.exports = {
 
         // DOM modifications
         if (elementMap.canvas !== null) {
-            element.style.width = elementMap.canvas.width.toString() + 'px';
+            element.style.width = elementMap.canvas.width.toString() + "px";
         } else {
             // TODO: create the canvas element ?, same for tittle ?
-            throw 'canvas element does not exist'
+            throw "canvas element does not exist";
         }
 
         if (elementMap.tittle !== null) {
             switch (options.tittle_pos) {
-                case 'top-left':
-                    elementMap.tittle.style.textAlign = 'left';
+                case "top-left":
+                    elementMap.tittle.style.textAlign = "left";
                     break;
-                case 'top-center':
-                    elementMap.tittle.style.textAlign = 'center';
+                case "top-center":
+                    elementMap.tittle.style.textAlign = "center";
                     break;
-                case 'top-right':
-                    elementMap.tittle.style.textAlign = 'right';
+                case "top-right":
+                    elementMap.tittle.style.textAlign = "right";
             }
             elementMap.tittle.innerHTML = options.tittle;
         }
