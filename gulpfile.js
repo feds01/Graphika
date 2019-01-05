be4c5c44e18fbd98e41298e35d4526d66bad2202
Alex <alexander.fedotov.uk@gmail.com>
2019-01-05 17:28:52 +0000

    [builder] generating sourceMaps. WIP
    Signed-off-by: Alex <alexander.fedotov.uk@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,66 @@
-const gulp = require("gulp"),
-    browserify = require("browserify"),
-    buffer = require("vinyl-buffer"),
-    uglifyjs = require("uglify-es"),
-    composer = require("gulp-uglify/composer"),
-    size = require("gulp-size"),
-    source = require("vinyl-source-stream");
+const size = require("gulp-size");
 
-let minify = composer(uglifyjs, console);
+/* jshint strict: false */
+/* globals require, console */
+let gulp = require("gulp");
+let exit = require("gulp-exit");
 
-gulp.task("js", () => {
-    return browserify("src/graph.js", {standalone: "Graph"})
-        .bundle()
-        .on("error", function (err) {
-            console.log("Error : " + err.message);
-            this.emit("end");
-        })
-        .pipe(source("graph.min.js"))
-        .pipe(buffer())
-        .pipe(minify())
-        .pipe(size({"gzip": true}))
-        .pipe(gulp.dest("./dist"));
+let browserify = require("browserify");
+let watchify = require("watchify");
+let babelify = require("babelify");
+
+let source = require("vinyl-source-stream");
+let buffer = require("vinyl-buffer");
+
+let rename = require("gulp-rename");
+let uglify = require("gulp-uglify");
+let sourcemaps = require("gulp-sourcemaps");
+
+
+function compile(watch) {
+    let bundle = watchify(browserify("./src/graph.js", {debug: true}).transform(babelify, {
+        // Use all of the ES2015 spec
+        presets: ["es2015"],
+        sourceMaps: true
+    }));
+
+    function reBundle() {
+        return bundle
+            .bundle()
+            .on("error", function (err) {
+                console.error(err);
+                this.emit("end");
+            })
+            .pipe(source("build.js"))
+            .pipe(buffer())
+            .pipe(rename("graph.min.js"))
+            .pipe(sourcemaps.init({loadMaps: true}))
+            .pipe(uglify())
+            .pipe(sourcemaps.write("./"))
+            .pipe(gulp.dest("./dist"));
+    }
+
+    if (watch) {
+        bundle.on("update", function () {
+            console.log("-> bundling...");
+            reBundle();
+        });
+
+        reBundle();
+    } else {
+        reBundle().pipe(exit());
+    }
+}
+
+function watch() {
+    return compile(true);
+}
+
+gulp.task("build", function () {
+    return compile();
+});
+gulp.task("watch", function () {
+    return watch();
 });
 
-
-gulp.task("watch", () => {
-    gulp.watch("src/**/*.js", gulp.series(["js"]));
-});
-
-
-gulp.task("default", gulp.series(["js", "watch"]));
+gulp.task("default", ["watch"]);
