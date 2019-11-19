f30d5306b86de525f69cdd218040822480e488be
Alex <alexander.fedotov.uk@gmail.com>
2019-11-19 16:58:58 +0000

    Improved quality of canvas drawings by factoring in device pixel ratio and doing some canvas magic by translating image by half a pixel before drawing and back after.
    Signed-off-by: Alex <alexander.fedotov.uk@gmail.com>

--- src/utils.js
+++ src/utils.js
@@ -59,9 +59,28 @@ module.exports = {
     },
 
 
-    isUndefOrNaN: function (o) {
+
+    isUndefOrNaN(o) {
         return isNaN(o) || typeof o === "undefined";
 
+    },
+
+    setupCanvas(canvas) {
+        // Get the device pixel ratio, falling back to 1.
+        const dpr = window.devicePixelRatio || 1;
+        // Get the size of the canvas in CSS pixels.
+        const rect = canvas.getBoundingClientRect();
+        // Give the canvas pixel dimensions of their CSS and set canvas dimensions to
+        // size * the device pixel ratio.
+
+        canvas.width = rect.width * dpr;
+        canvas.height = rect.height * dpr;
+        const ctx = canvas.getContext("2d");
+
+        // Scale all drawing operations by the dpr, so you
+        // don't have to worry about the difference.
+        ctx.scale(dpr, dpr);
+        return ctx;
     },
 
     isUndefOrNull: isUndefOrNull
