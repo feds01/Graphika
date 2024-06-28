7b15141f38dd37eeb7dc11f83a235fed0279ab58
Alexander Fedotov <alexander.fedotov.uk@gmail.com>
2024-06-27 20:05:58 -0400

    utils(array): tidy up docs

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -14,8 +14,8 @@ type NumberArray = Array<number> | Float64Array;
 /**
  * fills an empty array from 0 to size with integers and then returns the new array.
  *
- * @param {number} size Fill the array up to the given size
- * @returns {Array<number>} array with numbers 0 up to size
+ * @param size Fill the array up to the given size.
+ * @returns array with numbers 0 up to size.
  * */
 export function fillRange(size: number): Array<number> {
     return Array.from(Array(size).keys());
@@ -25,9 +25,8 @@ export function fillRange(size: number): Array<number> {
  * Returns the longest string within a given array. It does not return the actual length
  * of the longest item, just the longest item.
  *
- * @param {Array<string>} arr Array of strings to find the longest item
- *
- * @returns {string} Longest string from the array
+ * @param arr - Array of strings to find the longest item.
+ * @returns Longest string from the array.
  * */
 export function longest(arr: string[]): string {
     return arr.reduce((a, b) => {
@@ -40,11 +39,9 @@ export function longest(arr: string[]): string {
  * given index is zero or less than zero, the function will return the element at zero
  * rather than undefined.
  *
- * @template T
- * @param {Number} index Current position in the array
- * @param {Array<T>} data The array to index for the previous item
- *
- * @returns {T} the previous item from the array, the same one if the index is 0
+ * @param index - Current position in the array.
+ * @param data - The array to index for the previous item.
+ * @returns the previous item from the array, the same one if the index is 0.
  * */
 export function getPrevious<T>(index: number, data: T[]): T {
     return index <= 0 ? data[0] : data[index - 1];
@@ -55,11 +52,9 @@ export function getPrevious<T>(index: number, data: T[]): T {
  * given index is equal to the length of the array - 1, or larger, return the last element
  * of the array, rather than undefined.
  *
- * @template T
- * @param {Number} index Current position in the array
- * @param {Array<T>} arr The array to index for the next item
- *
- * @returns {T} the previous item from the array, the same one if the index is length of the array
+ * @param index - Current position in the array.
+ * @param arr - The array to index for the next item.
+ * @returns the previous item from the array, the same one if the index is length of the array.
  * */
 export function getNext<T>(index: number, arr: T[]): T {
     return arr[index >= arr.length - 1 ? arr.length - 1 : index + 1];
@@ -68,9 +63,8 @@ export function getNext<T>(index: number, arr: T[]): T {
 /**
  * Get all only negative values from a given array.
  *
- * @param {Array<number>} arr The array to filter out positive and zero values from.
- *
- * @returns {Array<number>} Negative only items
+ * @param arr - The array to filter out positive and zero values from.
+ * @returns Negative only items.
  * */
 export function negativeValues(arr: NumberArray): NumberArray {
     return arr.filter(function (value) {
@@ -81,9 +75,8 @@ export function negativeValues(arr: NumberArray): NumberArray {
 /**
  * Get all unique values from a given array.
  *
- * @param {Array<*>} arr The array to convert into a set
- *
- * @returns {Set<*>} the set of the array
+ * @param arr - The array to convert into a set.
+ * @returns the set of the array.
  */
 export function uniqueValues<T>(arr: T[]): Set<T> {
     return new Set(arr);
@@ -91,11 +84,10 @@ export function uniqueValues<T>(arr: T[]): Set<T> {
 
 /**
  * Function to get maximum element within array, we don't want to
- * use Math.max if it is a large array
+ * use Math.max if it is a large array.
  *
- * @param {Array<Number>} arr Source array
- *
- * @returns {Number} largest number in the array
+ * @param arr - Source array.
+ * @returns largest number in the array.
  */
 export function getMax(arr: number[] | Float64Array): number {
     let len = arr.length;
@@ -108,12 +100,10 @@ export function getMax(arr: number[] | Float64Array): number {
 }
 
 /**
- * Function to get maximum element within array, we don't want to
- * use Math.max if it is a large array
+ * Function to get maximum element within array.
  *
- * @param {Array<Number>} arr Source array
- *
- * @returns {Number} smallest number in the array
+ * @param arr - Source array.
+ * @returns Smallest number in the array.
  */
 export function getMin(arr: number[]): number {
     let len = arr.length;
@@ -126,13 +116,10 @@ export function getMin(arr: number[]): number {
 }
 
 /**
- * Function to get maximum and minimum element within array, this will get both elements
- * within a single iteration of the array instead of calling both min and max and doing
- * double the work.
+ * Function to get maximum and minimum element within array.
  *
- * @param {Array<Number>} arr Source array
- *
- * @returns {{min: Number, max: Number}} smallest and largest numbers within the array
+ * @param arr - Source array.
+ * @returns smallest and largest numbers within the array.
  */
 export function getMinMax(arr: NumberArray): { min: number; max: number } {
     let min = arr[0];
@@ -147,10 +134,11 @@ export function getMinMax(arr: NumberArray): { min: number; max: number } {
 }
 
 /**
- * Array sum function
  *
- * @param {Array<Number>} arr Number array to be summed.
- * @returns {Number} sum of the array
+ * Array sum function.
+ *
+ * @param arr - Array to be summed.
+ * @returns sum of the array.
  */
 export function sum(arr: number[]): number {
     return arr.reduce((a, b) => a + b, 0);
@@ -159,9 +147,8 @@ export function sum(arr: number[]): number {
 /**
  * Get all only positive and zero values from a given array.
  *
- * @param {Array<Number>} array The array that is to be filtered
- *
- * @returns {Array<Number>} the array with only natural numbers
+ * @param array - The array that is to be filtered.
+ * @returns the array with only natural numbers.
  *  */
 export function nonNegativeValues(array: NumberArray): NumberArray {
     return array.filter((value) => value >= 0);
